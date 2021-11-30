e95ccaa8d3e3353941cb68a7efabc7db76df47c4
Benaiah Catheraso <benaiah@itigo.ch>
2021-11-30 09:10:19 +0100

    initial commit

---
+++ ReadMe.md
@@ -0,0 +1,22 @@
+﻿# Chocolatey Package: Dialogik
+
+## Description
+
+Dialogik is a payroll accounting Software
+
+## Installation
+
+```ps1
+choco install dialogik
+```
+
+
+## Author
+
+- Benaiah Matthew Catherasoo
+- [Open Circle AG](https://www.open-circle.ch)
+
+
+## Copyright
+
+(c) 2021, Open Circle AG
---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://www.dialogikfile.ch/UpdateSetup/Setup.DialogikLohn.2021.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  softwareName  = 'dialogik*' 
+  checksum      = 'B09CFC63CB21DCE06C6FAD5E99D8DDDF3F2E5BD578F761EB4DCB5553E50881B2'
+  checksumType  = 'sha256' 
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" 
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
